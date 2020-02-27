63e1a2924acd2c4f2379c07b3ceda175f9cb52ff
nhd36 <nhd36@drexel.edu>
2020-02-27 15:33:41 -0500

    Authentication and Registration
    First steps

--- app/src/main/java/com/example/mealbuddy/userAuth/LoginPage.java
+++ app/src/main/java/com/example/mealbuddy/userAuth/LoginPage.java
@@ -1,16 +1,77 @@
 package com.example.mealbuddy.userAuth;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.mealbuddy.mainPage.mainPage;
 import com.example.mealbuddy.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginPage extends AppCompatActivity {
+    EditText SIEmail, SIPass;
+    Button SIButt;
+    TextView SITxt, Forget;
+    FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+
+        SIEmail = findViewById(R.id.SignInEmail);
+        SIPass = findViewById(R.id.SignInPassword);
+        SIButt = findViewById(R.id.SignInButt);
+        SITxt = findViewById(R.id.createTxt);
+        Forget = findViewById(R.id.ForgetPassword);
+
+        fAuth = FirebaseAuth.getInstance();
+
+        SIButt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = SIEmail.getText().toString().trim();
+                String pass = SIPass.getText().toString().trim();
+
+                if (email.isEmpty()){
+                    SIEmail.setError("Please enter email");
+                }
+
+                if (pass.isEmpty()){
+                    SIPass.setError("Please enter password");
+                }
+
+                fAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(LoginPage.this, "Login Successfully", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), mainPage.class));
+                        }
+                        else{
+                            Toast.makeText(LoginPage.this,"Error, please try again letter" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+            }
+        });
+
+        SITxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), SignUp.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/mealbuddy/userAuth/SignUp.java
+++ app/src/main/java/com/example/mealbuddy/userAuth/SignUp.java
@@ -1,16 +1,133 @@
 package com.example.mealbuddy.userAuth;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 import com.example.mealbuddy.R;
+import com.example.mealbuddy.mainPage.mainPage;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class SignUp extends AppCompatActivity {
+    EditText SUName, SUEmail, SUPass, SUPass2;
+    Button SUButt;
+    TextView SUText;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userID;
+    CheckBox SUCheck;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        SUName = findViewById(R.id.SignUpName);
+        SUEmail = findViewById(R.id.SignUpEmail);
+        SUPass = findViewById(R.id.SignUpPassword1);
+        SUPass2 = findViewById(R.id.SignUpPassword2);
+        SUButt = findViewById(R.id.SignUpButt);
+        SUText = findViewById(R.id.SignUpTxt);
+        SUCheck = findViewById(R.id.checkBox);
+
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+
+        SUButt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String email = SUEmail.getText().toString().trim();
+                final String password = SUPass.getText().toString().trim();
+                String password2 = SUPass2.getText().toString().trim();
+                final String name = SUName.getText().toString().trim();
+
+                if(email.isEmpty()){
+                    SUEmail.setError("Please enter email");
+                    return;
+                }
+
+                if(!email.contains("@drexel.edu")){
+                    SUPass.setError("Please use your Drexel Password for this Account");
+                    return;
+                }
+
+                if(password.isEmpty()){
+                    SUPass.setError("Please enter password");
+                    return;
+                }
+
+                if(password.length()<10){
+                    SUPass.setError("Password must be longer than 10");
+                    return;
+                }
+
+                if(!password2.equals(password)){
+                    SUPass2.setError("The second password must match the first password");
+                    return;
+                }
+
+                if(!SUCheck.isChecked()){
+                    SUCheck.setError("Please accept our privacy policy");
+                    return;
+                }
+
+                if(fAuth.getCurrentUser() != null){
+                    startActivity(new Intent(getApplicationContext(), mainPage.class));
+                    finish();
+                }
+
+                fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(SignUp.this, "Successfully Registered", Toast.LENGTH_SHORT).show();
+                            userID = fAuth.getCurrentUser().getUid();
+                            DocumentReference documentReference = fStore.collection("users").document(userID);
+                            Map <String, Object> user = new HashMap<>();
+                            user.put("first name", name);
+                            user.put("email", email);
+                            user.put("password", password);
+                            documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Log.d("TAG", "On Success: User profile is created for " + userID);
+                                }
+                            });
+                            startActivity(new Intent(getApplicationContext(), emailAuth.class));
+                        }
+                        else {
+                            Toast.makeText(SignUp.this, "Error, please try again" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+            }
+        });
+
+        SUText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), LoginPage.class));
+            }
+        });
+
     }
 }
