458bbb6946bc13c673fea62ab951289b326a9e50
Akshay Vaswani <34140914+AkshayVaswani@users.noreply.github.com>
2020-03-11 23:36:16 -0400

    made time list work

--- app/src/main/java/com/example/mealbuddy/mainPage/findAMealBuddy/time.java
+++ app/src/main/java/com/example/mealbuddy/mainPage/findAMealBuddy/time.java
@@ -15,11 +15,21 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class time extends AppCompatActivity {
 
@@ -35,19 +45,65 @@ public class time extends AppCompatActivity {
         liveList = new ArrayList<>();
         CollectionReference coll = fStore.collection("Times");
 
+
+//        String[] quarterHours = {"00","15","30","45"};
+//        List<String> timesMil = new ArrayList<>();
+//        List<String> timesNorm = new ArrayList<>();
+//        for(int i = 7; i < 22; i++) {
+//            for(int j = 0; j < 4; j++) {
+//                String time = i + ":" + quarterHours[j];
+//                if(i < 10) {
+//                    time = "0" + time;
+//                }
+//                timesMil.add(time);
+//            }
+//
+//        }
+//        Log.d("TAG2", timesMil.toString());
+//        DateFormat inFormat = new SimpleDateFormat("HH:mm");
+//        DateFormat outFormat = new SimpleDateFormat("hh:mm aa");
+//        Date date = null;
+//        String output = null;
+//        for(int i = 0; i < timesMil.size(); i++) {
+//            try {
+//                date = inFormat.parse(timesMil.get(i));
+//                output = outFormat.format(date);
+//                timesNorm.add(output);
+//                Log.d("TAG2", timesNorm.get(i));
+//
+//            } catch (ParseException pe) {
+//                pe.printStackTrace();
+//            }
+//        }
+//
+//        for(int i = 0; i < timesMil.size(); i++){
+//            Map<String, Object> tim = new HashMap<>();
+//            tim.put("normalTime", timesNorm.get(i));
+//            tim.put("milTime", timesMil.get(i));
+//            tim.put("Order", i);
+//            coll.document(timesNorm.get(i)).set(tim);
+//        }
+//        coll.orderBy("Order", Query.Direction.DESCENDING);
+//        final Map<Integer, String> collList = new HashMap<>();
         coll.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
                 if (task.isSuccessful()){
                     for(QueryDocumentSnapshot documentSnapshot : task.getResult()){
-                        liveList.add(new timeList(documentSnapshot.getString("Time")));
+//                        collList.put(documentSnapshot.getData)
+                        int temp = documentSnapshot.getLong("Order").intValue();
+                        liveList.add(new timeList(documentSnapshot.getString("normalTime"), temp));
+
+
                         //Log.d("TAG2", documentSnapshot.getString("Time"));
                     }
                 }
+
+                Collections.sort(liveList);
                 customAdapter adapter = new customAdapter(time.this, R.layout.activity_time_template, liveList);
 
                 listView = findViewById(R.id.listView);
-                Log.d("TAG2", liveList.get(3).getTime());
+
                 listView.setAdapter(adapter);
                 listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                     @Override
--- app/src/main/java/com/example/mealbuddy/mainPage/findAMealBuddy/timeList.java
+++ app/src/main/java/com/example/mealbuddy/mainPage/findAMealBuddy/timeList.java
@@ -1,16 +1,27 @@
 package com.example.mealbuddy.mainPage.findAMealBuddy;
 
-public class timeList {
+import java.util.Collection;
+
+public class timeList implements Comparable {
 
     public String getTime() {
         return time;
     }
 
     String time;
+    int order;
 
-    public timeList(String time){
+    public int getOrder(){ return order; }
+
+    public timeList(String time, int order){
         this.time = time;
+        this.order = order;
     }
 
 
+    @Override
+    public int compareTo(Object compareOrd) {
+        int compareOrder =((timeList)compareOrd).getOrder();
+        return this.order - compareOrder;
+    }
 }
